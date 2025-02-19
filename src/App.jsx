17fedb350724a11ab28c209f8208dbf3acf07a57
gneo0 <geo_nio@hotmail.com>
2025-02-19 22:43:30 +0200

    Add Responsive Design and add routing on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import Homepage from './pages/Homepage'
+import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
-    <div>
-    <p class="text-base sm:text-xl md:text-2xl lg:text-3xl">This is responsive text!</p>
+    <>
+      <Homepage />
+    </>
+  );
+};
 
-      <Homepage/>
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/Components/ImageGrid.jsx
+++ src/Components/ImageGrid.jsx
@@ -1,44 +1,33 @@
-import React from "react";
-
 const images = [
-  { src: "/Images/5.png", colSpan: "col-span-2", rowSpan: "row-span-1" }, 
-  { src: "/Images/11.png", colSpan: "col-span-1", rowSpan: "row-span-1" }, 
-  { src: "/Images/10.png", colSpan: "col-span-1", rowSpan: "row-span-1" }, 
+  { src: "/Images/5.png", colSpan: "col-span-2", rowSpan: "row-span-1" },
+  { src: "/Images/11.png", colSpan: "col-span-1", rowSpan: "row-span-1" },
+  { src: "/Images/10.png", colSpan: "col-span-1", rowSpan: "row-span-1" },
   { src: "/Images/9.png", colSpan: "col-span-1", rowSpan: "row-span-2" }, // Tall image
   { src: "/Images/8.png", colSpan: "col-span-1", rowSpan: "row-span-1" }, // Wide image
-  { src: "/Images/5.png", colSpan: "col-span-1", rowSpan: "row-span-1" }, 
-  { src: "/Images/7.png", colSpan: "col-span-1", rowSpan: "row-span-1" }, 
+  { src: "/Images/5.png", colSpan: "col-span-1", rowSpan: "row-span-1" },
+  { src: "/Images/7.png", colSpan: "col-span-1", rowSpan: "row-span-1" },
 ];
 
 const ImageGrid = () => {
   return (
     <>
-
-    <div className="max-w-full w-[90%] lg:w-[80%] mx-auto py-8 px-4 md:px-8 lg:px-16 bg-blue-100 translate-y-[-200px]">
-      <div className="grid grid-cols-3 gap-4 md:gap-6 grid-rows-2">
-        {images.map((image, index) => (
-          <div key={index} className={`${image.colSpan} ${image.rowSpan}`}>
-            <img
-              src={image.src}
-              alt={`Gallery ${index + 1}`}
-              className="w-full h-full object-cover rounded-lg"
-            />
-          </div>
-        ))}
+      <div className="max-w-5xl w-[90%] lg:w-[80%] mx-auto py-8 px-4 md:px-8 lg:px-16 bg-blue-100">
+        <ul className="grid grid-cols-3 gap-4 md:gap-6 grid-rows-2">
+          {images.map((image, index) => (
+            <li key={index} className={`${image.colSpan} ${image.rowSpan}`}>
+              <img
+                src={image.src}
+                alt={`Gallery ${index + 1}`}
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </li>
+          ))}
+        </ul>
       </div>
-      
-    </div>
-    <div className="w-full">
-  <img
-    src="Images/3.png"
-    alt=""
-    className="w-full h-auto object-cover"
-  />
-</div>
-
+      <div className="w-full">
+        <img src="Images/3.png" alt="" className="w-full max-w-7xl mx-auto" />
+      </div>
     </>
-    
-    
   );
 };
 
